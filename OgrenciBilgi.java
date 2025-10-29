460f4e28a306f80ef84b0853087f30faa812ecb1
250541064 <250541064@firat.edu.tr>
2025-10-29 20:24:08 +0300

    OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -29,10 +29,14 @@ public class OgrenciBilgiSistemi {
         System.out.print("Ogrenci AGNO (GPA): ");
         double ogrenciAgno = scanner.nextDouble();
 
-        System.out.printf("\nAd Soyad     : %s %s\n", ogrenciAdi, ogrenciSoyadi);
-        System.out.printf("Numara       : %d\n", ogrenciNumarasi);
-        System.out.printf("Yas          : %d\n", ogrenciYasi);
-        System.out.printf("AGNO (GPA)   : %.2f\n", ogrenciAgno);
+        if (ogrenciAgno < 0 || ogrenciAgno > 4) {
+            System.out.println("Hatali AGNO! Lutfen 0 ile 4 arasinda bir deger giriniz.");
+        } else {
+            System.out.printf("\nAd Soyad     : %s %s\n", ogrenciAdi, ogrenciSoyadi);
+            System.out.printf("Numara       : %d\n", ogrenciNumarasi);
+            System.out.printf("Yas          : %d\n", ogrenciYasi);
+            System.out.printf("AGNO (GPA)   : %.2f\n", ogrenciAgno);
+        }
 
         scanner.close();
     }
